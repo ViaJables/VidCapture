cf746c0554854dc0d676e8fd9e340dba2f0a18f5
John Brunsfeld <jbrunsfeld@clinkle.com>
2016-09-16 13:50:24 -0700

    second push

--- Whale/Recording/VideoHandler.swift
+++ Whale/Recording/VideoHandler.swift
@@ -1,5 +1,5 @@
 //
-//  CameraSessionController.swift
+//  VideoHandler.swift
 //  Whale
 //
 //  Created by John on 9/15/16.
@@ -9,54 +9,73 @@
 import Foundation
 import CoreMedia
 import AVFoundation
-import AssetsLibrary
-import MediaPlayer
 import CoreAudio
 import CoreFoundation
+import Photos
 
-class CameraSessionController: AVCaptureFileOutputRecordingDelegate {
-    static let sharedInstance = CameraSessionController()
+protocol VideoHandlerDelegate: class {
+    func didStartRecording()
+    func exportCompleted(url: NSURL!)
+}
+
+class VideoHandler: NSObject {
+    // Delegate
+    weak var delegate:VideoHandlerDelegate?
     
+    // Session
     var captureSession: AVCaptureSession?
+    
+    // Devices
     var videoCaptureDevice: AVCaptureDevice?
     var audioCaptureDevice: AVCaptureDevice?
     var videoInputDevice: AVCaptureDeviceInput?
     var audioInputDevice: AVCaptureDeviceInput?
-    var previewLayer: AVCaptureVideoPreviewLayer?
     
+    // File Outputs
     var movieFileOutput: AVCaptureMovieFileOutput?
     var outputPath = NSTemporaryDirectory() as String
     
-    var totalSeconds: Float64 = 10.00
-    var framesPerSecond:Int32 = 30
-    
-    var maxDuration: CMTime?
-    var toggleCameraSwitch: UIButton = UIButton()
-    
+    // Collections of Assets (for merging)
     var videoAssets = [AVAsset]()
     var assetURLs = [String]()
+
+    // Settings
+    var totalSeconds: Float64 = 60.00
+    var framesPerSecond:Int32 = 30
+    var maxDuration: CMTime = CMTime(seconds: 60, preferredTimescale: 30)
+    var remainingTime : NSTimeInterval = 60.0
     
-    var incInterval: NSTimeInterval = 0.05
-    var timer: NSTimer?
-    var stopRecording: Bool = false
-    var remainingTime : NSTimeInterval = 10.0
-    var oldX: CGFloat = 0
-    var appendix: Int32 = 1
+    var filePartAppendix: Int32 = 1
     var defaultTrimDuration: Int = 10
     
+    override init() {
+        super.init()
+        setupCapturing()
+    }
+    
     func setupCapturing() {
+        //TODO - This takes too long, need to research correct way to do this on background thread
+        
+        captureSession = AVCaptureSession()
+        captureSession?.beginConfiguration()
         if let videoCaptureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo) {
-            videoInputDevice =  try? AVCaptureDeviceInput(device: videoCaptureDevice )
+            videoInputDevice = try? AVCaptureDeviceInput(device: videoCaptureDevice )
             captureSession?.addInput(videoInputDevice)
         }
         
-        if let audioCaptureDevice = AVCaptureDevice.devicesWithMediaType(AVMediaTypeAudio)[0] as? AVCaptureDevice {
-            audioInputDevice =  try? AVCaptureDeviceInput(device: audioCaptureDevice )
+        
+        if let audioCaptureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeAudio) {
+            audioInputDevice = try? AVCaptureDeviceInput(device: audioCaptureDevice )
             captureSession?.addInput(audioInputDevice)
         }
-    }
-    
-    func beginRecording() {
+        
+        movieFileOutput = AVCaptureMovieFileOutput()
+        maxDuration = CMTimeMakeWithSeconds(totalSeconds, 1)
+        movieFileOutput?.maxRecordedDuration = maxDuration
+        
+        captureSession?.addOutput(movieFileOutput)
+        captureSession?.commitConfiguration()
+        
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
             
             self.captureSession?.startRunning()
@@ -64,27 +83,154 @@ class CameraSessionController: AVCaptureFileOutputRecordingDelegate {
         })
     }
     
-    func endRecording() {
-        
-    }
-    
-    func connectionIsActive(connections: [AVCaptureConnection], mediaType: String) -> AVCaptureConnection {
-        
-        for connection in connections {
-            
-            for port in connection.inputPorts {
-                
-                if(port.mediaType  == mediaType) {
-                    
-                    
-                    return connection
-                }
-            }
-            
+    func createViewPortLayer(frame: CGRect) -> AVCaptureVideoPreviewLayer?  {
+        if let viewPortLayer = AVCaptureVideoPreviewLayer(session: captureSession) {
+            viewPortLayer.frame = frame
+            viewPortLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+            return viewPortLayer
         }
         
-        return connections[0] as AVCaptureConnection
+        return nil
+    }
+    
+    func beginRecording() {
+        
+        
+        let outputFilePath = outputPath + "output-\(filePartAppendix).mov"
+        filePartAppendix += 1
+        let outputURL = NSURL(fileURLWithPath: outputFilePath)
+        let fileManager = NSFileManager.defaultManager()
+        if(fileManager.fileExistsAtPath(outputFilePath)) {
+            
+            do {
+                try fileManager.removeItemAtPath(outputFilePath)
+            } catch _ {
+            }
+        }
+        
+        movieFileOutput?.startRecordingToOutputFileURL(outputURL, recordingDelegate: self)
+    }
+    
+    func endRecording() {
+        movieFileOutput?.stopRecording()
+    }
+    
+    func flipCamera() {
+        var newInputDevice: AVCaptureDeviceInput = videoInputDevice!
+        let position: AVCaptureDevicePosition? = videoInputDevice?.device.position
+        var newDevice: AVCaptureDevice?
+        if(position == AVCaptureDevicePosition.Back) {
+            newDevice = cameraWithPosition(AVCaptureDevicePosition.Front)
+            newInputDevice = try! AVCaptureDeviceInput(device: newDevice)
+        } else if(position == AVCaptureDevicePosition.Front) {
+            newDevice = cameraWithPosition(AVCaptureDevicePosition.Back)
+            newInputDevice = try! AVCaptureDeviceInput(device: newDevice)
+        }
+        
+        captureSession?.beginConfiguration()
+        captureSession?.removeInput(videoInputDevice)
+        captureSession?.addInput(newInputDevice)
+        videoInputDevice = newInputDevice
+        captureSession?.commitConfiguration()
     }
 
+    func cameraWithPosition(position: AVCaptureDevicePosition) -> AVCaptureDevice {
+        
+        var rv: AVCaptureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        let devices = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
+        for device in devices {
+            if(device.position == position) {
+                rv = device as! AVCaptureDevice
+            }
+        }
+        return rv
+    }
     
+    func reset() {
+        // Delete what we've written
+        for assetURL in assetURLs {
+            
+            if(NSFileManager.defaultManager().fileExistsAtPath(assetURL)) {
+                
+                do {
+                    try NSFileManager.defaultManager().removeItemAtPath(assetURL)
+                } catch _ {
+                }
+            }
+        }
+        
+        videoAssets.removeAll(keepCapacity: false)
+        assetURLs.removeAll(keepCapacity: false)
+        filePartAppendix = 1
+        remainingTime = 60.00
+    }
+
+    //MARK: video merging
+    
+    func mergeVideos() {
+        let composition = AVMutableComposition()
+        
+        let firstTrack:AVMutableCompositionTrack = composition.addMutableTrackWithMediaType(AVMediaTypeVideo, preferredTrackID: CMPersistentTrackID())
+        
+        let audioTrack: AVMutableCompositionTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: CMPersistentTrackID())
+        
+        var insertTime: CMTime = kCMTimeZero
+        
+        for asset in videoAssets {
+            do {
+                try firstTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, asset.duration), ofTrack: asset.tracksWithMediaType(AVMediaTypeVideo)[0] , atTime: insertTime)
+            } catch _ {
+            }
+            do {
+                try audioTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, asset.duration), ofTrack: asset.tracksWithMediaType(AVMediaTypeAudio)[0] , atTime: insertTime)
+            } catch _ {
+            }
+            
+            insertTime = CMTimeAdd(insertTime, asset.duration)
+        }
+        firstTrack.preferredTransform = CGAffineTransformMakeRotation(CGFloat(M_PI_2))
+        
+        //get path of newly merged video
+        let documentsPath : String = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,.UserDomainMask,true)[0]
+        let destinationPath: String = documentsPath + "/mergeVideo-\(arc4random()%1000).mov"
+        let videoPath: NSURL = NSURL(fileURLWithPath: destinationPath as String)
+        if let exporter: AVAssetExportSession = AVAssetExportSession(asset: composition, presetName:AVAssetExportPresetHighestQuality) {
+            exporter.outputURL = videoPath
+            exporter.outputFileType = AVFileTypeQuickTimeMovie
+            exporter.shouldOptimizeForNetworkUse = true
+            exporter.timeRange = CMTimeRangeMake(kCMTimeZero, CMTimeMakeWithSeconds(Float64(totalSeconds),framesPerSecond))
+            exporter.exportAsynchronouslyWithCompletionHandler({
+                
+                // finished (call on main thread)
+                dispatch_async(dispatch_get_main_queue(),{
+                    self.exportDidFinish(exporter)
+                })
+                
+            })
+        }
+    }
+    
+    func exportDidFinish(session: AVAssetExportSession) {
+        if let outputURL: NSURL = session.outputURL {
+            //TODO - Write output to PHLibrary
+            self.delegate?.exportCompleted(outputURL)
+        }
+    }
+}
+
+extension VideoHandler: AVCaptureFileOutputRecordingDelegate {
+    func captureOutput(captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAtURL outputFileURL: NSURL!, fromConnections connections: [AnyObject]!, error: NSError!) {
+        // Append asset onto array
+        let asset : AVURLAsset = AVURLAsset(URL: outputFileURL, options: nil)
+        videoAssets.append(asset)
+        assetURLs.append(outputFileURL.path!)
+        
+        //Update remaining time
+        let duration = CMTimeGetSeconds(asset.duration)
+        remainingTime = remainingTime - duration
+    }
+    
+    func captureOutput(captureOutput: AVCaptureFileOutput!, didStartRecordingToOutputFileAtURL fileURL: NSURL!, fromConnections connections: [AnyObject]!) {
+        delegate?.didStartRecording()
+    }
 }
--- Whale/ViewController.swift
+++ Whale/ViewController.swift
@@ -7,19 +7,132 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var progressView: UIView?
+    @IBOutlet weak var viewPort: UIView?
+    @IBOutlet weak var deleteButton: UIButton?
+    @IBOutlet weak var recordButton: UIButton?
+    @IBOutlet weak var nextButton: UIBarButtonItem?
+    
+    var longPressRecognizer: UILongPressGestureRecognizer!
+    var videoHandler: VideoHandler!
+    
+    var progressTimer: NSTimer? = nil
+    var finishedRecording = false
+    var remainingTime: NSTimeInterval = 0.0
+    var progressBarProgressX: CGFloat = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Record"
+        videoHandler = VideoHandler()
+        videoHandler.delegate = self
+        nextButton?.enabled = false
+        deleteButton?.hidden = true
+        progressView?.layer.borderColor = UIColor.blackColor().CGColor
+        progressView?.layer.borderWidth = 1.0
+        recordButton?.layer.borderColor = UIColor.lightGrayColor().CGColor
+        recordButton?.layer.borderWidth = 15.0
+        recordButton?.layer.cornerRadius = 60
+
+        // Add a recognizer to the button for long press (could also do with touch methods)
+        longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.recordPressed))
+        recordButton?.addGestureRecognizer(longPressRecognizer)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // Add the viewport
+        if let viewPortLayer = videoHandler.createViewPortLayer((self.viewPort?.bounds)!) {
+            viewPort?.layer.addSublayer(viewPortLayer)
+        }
+    }
+    
+    func recordPressed(recognizer: UILongPressGestureRecognizer) {
+        if recognizer.state == UIGestureRecognizerState.Began {
+            videoHandler.beginRecording()
+            recordButton?.layer.borderColor = UIColor(red: 170.0/255.0, green: 67.0/255.0, blue: 170.0/255.0, alpha: 1.0).CGColor            
+        } else if recognizer.state == UIGestureRecognizerState.Ended {
+            progressTimer?.invalidate()
+            videoHandler.endRecording()
+            progressTimer?.invalidate()
+            progressBarProgressX += 1
+            recordButton?.layer.borderColor = UIColor.lightGrayColor().CGColor
+        }
+    }
+    
+    @IBAction func flipCamera() {
+        videoHandler.flipCamera()
+    }
+    
+    @IBAction func deletePressed() {
+        videoHandler.reset()
+        progressBarProgressX = 0
+        nextButton?.enabled = false
+        deleteButton?.hidden = true
+        clearProgressBar()
+    }
+    
+    @IBAction func nextPressed() {
+        //TODO - Should add in an activity indicator here
+        videoHandler.mergeVideos()
+    }
+    
+    private func startProgressTimer() {
+        
+        progressTimer = NSTimer(timeInterval: 0.05, target: self, selector: #selector(ViewController.updateProgress), userInfo: nil, repeats: true)
+        
+        NSRunLoop.currentRunLoop().addTimer(progressTimer!, forMode: NSDefaultRunLoopMode)
+    }
+    
+    func updateProgress() {
+        let progressProportion: CGFloat = CGFloat(0.05 / 60.0)
+        let progressInc: UIView = UIView()
+        progressInc.backgroundColor = UIColor(red: 170.0/255.0, green: 67.0/255.0, blue: 170.0/255.0, alpha: 1.0)
+        let newWidth = (progressView?.frame.width)! * progressProportion
+        progressInc.frame = CGRect(x: progressBarProgressX , y: 0, width: newWidth, height: progressView!.frame.height)
+        progressBarProgressX = progressBarProgressX + newWidth
+        progressView?.addSubview(progressInc)
+    }
+    
+    private func addSegmentToProgressBar() {
+        
+    }
+    
+    private func clearProgressBar() {
+        if let subviews = progressView?.subviews {
+            for subview in subviews {
+                
+                subview.removeFromSuperview()
+            }
+        }
+    }
+}
 
-
+extension ViewController: VideoHandlerDelegate {
+    func didStartRecording() {
+        deleteButton?.hidden = false
+        nextButton?.enabled = true
+        startProgressTimer()
+    }
+    
+    func exportCompleted(url: NSURL!) {
+        let player = AVPlayer(URL: url)
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        self.presentViewController(playerViewController, animated: true) {
+            playerViewController.player?.play()
+        }
+    }
 }
 
